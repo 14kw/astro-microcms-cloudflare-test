850d13c9efbd86fae726a4917864299566606bd3
14kw <14codemail@gmail.com>
2023-01-30 17:18:05 +0900

    debug: Add vite define env

--- prerender-test/astro.config.mjs
+++ prerender-test/astro.config.mjs
@@ -5,5 +5,15 @@ import cloudflare from "@astrojs/cloudflare";
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  adapter: cloudflare()
+  adapter: cloudflare(),
+  vite: {
+    define: {
+        "process.env.MICROCMS_SERVICE_DOMAIN": JSON.stringify(
+            process.env.MICROCMS_SERVICE_DOMAIN,
+        ),
+        "process.env.MICROCMS_API_KEY": JSON.stringify(
+            process.env.MICROCMS_API_KEY,
+        ),
+    },
+  }
 });
