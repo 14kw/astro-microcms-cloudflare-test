1b68530fcbf6b29e9b44c5fffaeb0d2047665599
14kw <14codemail@gmail.com>
2023-02-28 16:21:00 +0900

    chore: Add node adapter

--- prerender-test/astro.config.mjs
+++ prerender-test/astro.config.mjs
@@ -3,17 +3,19 @@ import { defineConfig } from 'astro/config';
 import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
+import node from "@astrojs/node";
+
+// https://astro.build/config
 export default defineConfig({
   output: 'server',
-  adapter: cloudflare(),
+  adapter: node(),
   vite: {
     define: {
-        "process.env.MICROCMS_SERVICE_DOMAIN": JSON.stringify(
-            process.env.MICROCMS_SERVICE_DOMAIN,
-        ),
-        "process.env.MICROCMS_API_KEY": JSON.stringify(
-            process.env.MICROCMS_API_KEY,
-        ),
+      "process.env.MICROCMS_SERVICE_DOMAIN": JSON.stringify(process.env.MICROCMS_SERVICE_DOMAIN),
+      "process.env.MICROCMS_API_KEY": JSON.stringify(process.env.MICROCMS_API_KEY)
     },
+    ssr: {
+      noExternal: ["path-to-regexp"]
+    }
   }
 });
